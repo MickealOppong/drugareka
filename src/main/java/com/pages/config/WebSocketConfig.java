c1fb155ab90f4dd9dbc631caf3dd86cf87b922e1
mike oppong <mikeoppong@Mike.local>
2026-09-18 15:30:10 +0200

    v3

--- src/main/java/com/pages/config/WebSocketConfig.java
+++ src/main/java/com/pages/config/WebSocketConfig.java
@@ -16,8 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 // Safely allows your local server, your current frontend, and any potential backup subdomains
-                .setAllowedOriginPatterns("http://localhost:5173","https://pages-api-production-48f8.up.railway.app",
-                        "https://www.spotkac.com", "http://www.spotkac.com");
+                .setAllowedOriginPatterns("http://localhost:5173","https://drugareka-production.up.railway.app","https://store-drugareka-production.up.railway.app");
     }
 
 
